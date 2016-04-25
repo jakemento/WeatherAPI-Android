fd41f9e93e0c60a755918a1fa5349d5c24efe20b
Jake Steinberg <jakesteinberg@gmail.com>
2016-04-25 14:31:25 -0700

    upload inital files + basic location search

---
+++ app/src/main/java/com/epicodus/myweather/WeatherActivity.java
@@ -0,0 +1,25 @@
+package com.epicodus.myweather;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class WeatherActivity extends AppCompatActivity {
+    public static final String TAG = WeatherActivity.class.getSimpleName();
+    @Bind(R.id.locationTextView) TextView mLocationTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.setText("the city you entered is " + location + "!");
+    }
+}
